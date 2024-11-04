b83599a06bd6ca1053847d2a3e634ddee3f6fef8
Oleg Zholobov <zholobov.of@edu.spbstu.ru>
2024-11-04 19:02:37 +0300

    task6.1

--- matrix.cpp
+++ matrix.cpp
@@ -3,58 +3,102 @@
 
 void clear(int **t, size_t m)
 {
-    for (size_t i = 0; i < m; i += 1)
-    {
-        delete[] t[i];
-    }
-    delete[] t;
+  for (size_t i = 0; i < m; i += 1)
+  {
+    delete[] t[i];
+  }
+  delete[] t;
 }
 
 int **matrix(size_t m, size_t n)
 {
-    int **t = new int *[m];
-    size_t created = 0;
+  int **t = new int *[m];
+  size_t created = 0;
 
-    try
+  try
+  {
+    for (; created < m; created += 1)
     {
-        for (; created < m; created += 1)
-        {
-            t[created] = new int[n];
-        }
+      t[created] = new int[n];
     }
-    catch (const std::bad_alloc &e)
-    {
-        clear(t, created);
-        throw;
-    }
-    return t;
+  }
+  catch (const std::bad_alloc &e)
+  {
+    clear(t, created);
+    throw;
+  }
+  return t;
 }
 
 bool enter(int **t, size_t m, size_t n)
 {
-    for (size_t i = 0; i < m; i += 1)
+  for (size_t i = 0; i < m; i += 1)
+  {
+    for (size_t j = 0; j < n; j += 1)
     {
-        for (size_t j = 0; j < n; j += 1)
-        {
-            std::cin >> t[i][j];
-            if (!std::cin){
-                std::cerr << "Not a number\n";
-                return false;
-            }
-        }
+      std::cin >> t[i][j];
+      if (!std::cin)
+      {
+        std::cerr << "Not a number\n";
+        return false;
+      }
     }
-    return true;
+  }
+  return true;
 }
 
 void out(const int *const *t, size_t m, size_t n)
 {
-    for (size_t i = 0; i < m; i += 1)
+  for (size_t i = 0; i < m; i += 1)
+  {
+    std::cout << t[i][0];
+    for (size_t j = 1; j < n; j += 1)
     {
-        std::cout << t[i][0];
-        for (size_t j = 1; j < n; j += 1)
-        {
-            std::cout << " " << t[i][j];
-        }
-        std::cout << "\n";
+      std::cout << " " << t[i][j];
     }
+    std::cout << "\n";
+  }
+}
+
+Matrix::Matrix(size_t rows, size_t cols) {
+//  create(rows, cols);
+  rows_ = rows;
+  cols_ = cols;
+  data_ = ::matrix(rows, cols);
+}
+
+Matrix::Matrix(const Matrix& other) {
+  rows_ = other.rows_;
+  cols_ = other.cols_;
+  data_ = matrix(other.rows_, other.cols_);
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      data_[i][j] = other.data_[i][j];
+    }
+  }
+}
+
+Matrix::~Matrix() {
+  clear(rows_);
+}
+
+size_t Matrix::getRows() const {
+  return rows_;
+}
+
+size_t Matrix::getCols() const {
+  return cols_;
+}
+
+void Matrix::fill(int value) {
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      data_[i][j] = value;
+    }
+  }
+}
+
+void Matrix::resize(size_t new_rows, size_t new_cols) {
+  clear(rows_);
+  create(new_rows, new_cols);
 }
--- matrix.hpp
+++ matrix.hpp
@@ -7,4 +7,25 @@ int **matrix(size_t m, size_t n);
 void clear(int **t, size_t m);
 bool enter(int **t, size_t m, size_t n);
 void out(const int *const *t, size_t m, size_t n);
+
+class Matrix
+{
+  size_t rows_; // rows
+  size_t cols_; // cols
+  int **data_;
+
+public:
+  Matrix(size_t rows, size_t cols);
+  Matrix(const Matrix& other);
+  ~Matrix();
+  size_t getRows() const;
+  size_t getCols() const;
+  void fill(int value);
+  void resize(size_t new_rows, size_t new_cols);
+
+private:
+  void create(size_t rows, size_t cols);
+  void clear(size_t rows_to_clear);
+};
+
 #endif
